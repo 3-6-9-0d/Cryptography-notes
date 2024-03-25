3844b67e74889badbc1b6daee2e05c1f3a949bb8
3-6-9 <85024630+3-6-9-0d@users.noreply.github.com>
2024-03-26 01:00:45 +0530

    Create Vigenere_enc_dec.py

---
+++ Vigenere (polyalphabetic)/Vigenere_enc_dec.py
@@ -0,0 +1,57 @@
+ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def vigenere_encrypt(plaint_text, key):
+
+    #this is the text we want to encrypt + case-sensitive
+    plaint_text = plaint_text.upper()
+    key = key.upper()
+
+
+    cipher_text = ''
+
+    # it represents the index of the letters of the key
+    key_index = 0
+
+    for character in plaint_text:
+
+        index = (ALPHABET.find(character)+ALPHABET.find(key[key_index])) % len(ALPHABET)
+
+        cipher_text = cipher_text + ALPHABET[index] 
+
+        key_index = key_index + 1
+
+        if key_index == len(key):
+            key_index = 0
+
+    return cipher_text
+
+def vigenere_decrypt(cipher_text, key):
+
+    cipher_text = cipher_text.upper()
+    key = key.upper()
+
+    plain_text = ''
+
+    key_index = 0
+
+    for character in cipher_text:
+
+        index = (ALPHABET.find(character) - ALPHABET.find(key[key_index])) %len(ALPHABET)
+
+        plain_text = plain_text + ALPHABET[index]
+
+        key_index = key_index + 1
+
+        if key_index == len(key):
+            key_index = 0
+
+    return plain_text
+
+if __name__ == '__main__':
+    text = "CRYPTOGRAPHY IS QUITE IMPORTANT IN CRYPTOCURRENCIES"
+
+    cipher = vigenere_encrypt(text, 'ANIMAL')
+
+    print(vigenere_decrypt(cipher, 'ANIMAL'))
+    
+    print(cipher)
